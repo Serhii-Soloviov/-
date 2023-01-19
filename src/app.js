c1b0f3048ec70be956f02115384c27051fb57a33
Serhii Soloviov <115696868+Serhii-Soloviov@users.noreply.github.com>
2023-01-19 18:32:15 +0200

    'update'

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
-console.log('Hello world!');
-console.log('fuck yeah');
+// console.log('Hello world!');
+// console.log('fuck yeah');
+import * as numbur from './js/welcome';
+console.log(numbur.f);
+// import { a, b, c as d } from './js/welcome';
+// console.log(a, b, d);
+// console.log(d);
---
+++ src/js/welcome.js
@@ -0,0 +1,10 @@
+// export default function validatiomPasword(x) {
+//   return true;
+// };
+export const a = 5;
+export const b = 6;
+export const c = 7;
+// export default {a, b, c}
+export const f = function rest() {
+  return console.log('hello');
+};
